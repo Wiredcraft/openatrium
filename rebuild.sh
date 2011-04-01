420453f1b9162951a76a435581ea864a601b2f2a
Makara Wang <makara15@gmail.com>
2011-04-01 19:28:42 +0800

    fix

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-drush make --no-core --contrib-destination=. config/openatrium.make
+
+DIRS="atrium_features contrib custom developer l10n";
+for DIR in $DIRS; do
+  if [[ -d "modules/$DIR" ]]; then
+    rm -rf "modules/$DIR";
+  fi;
+done;
+
+DIRS="ginkgo rubik tao";
+for DIR in $DIRS; do
+  if [[ -d "themes/$DIR" ]]; then
+    rm -rf "themes/$DIR";
+  fi;
+done;
+
+drush make --no-core --contrib-destination=. config/openatrium.make -y;
